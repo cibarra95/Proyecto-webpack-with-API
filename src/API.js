f0a0ebcec1b72c24432039dce05bcd24518212e6
cibarra95 <cesarij07@gmail.com>
2021-03-08 14:54:11 -0600

    First commit

---
+++ src/API.js
@@ -0,0 +1,10 @@
+// Almacenar el arreglo de la trivia
+
+class arregloTrivia {
+    constructor(consulta = []){
+        this.consulta = consulta;
+    }
+    renderConsulta(){}
+}
+
+export default arregloTrivia;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,25 @@
-import './styles/main.scss';
-// importamos los estilos de la carpeta styles
+import './style/main.scss';
+
+// importamos los estilos de la carpeta style
+
+import Consulta from './API.js';
+import axios from 'axios';
+// const request = require('request');
+// traer la consulta
+
+const init = () => {
+    const consultaAPI = [];
+    // Todo: Consultar API
+
+    axios.get('https://opentdb.com/api.php?amount=10')
+        .then(function (response){
+            // handle success
+            console.log(response);
+        })
+        
+    const trivia = new Consulta
+    (consultaAPI);
+    console.log(trivia);
+};
+
+init();
