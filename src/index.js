2c23f73ef7931cccfe710d0bba4c531447638b54
cibarra95 <cesarij07@gmail.com>
2021-03-16 22:07:50 -0600

    Ejemplo pokedex API

--- src/index.js
+++ src/index.js
@@ -1,51 +1,40 @@
-import './styles/main.scss';
-// importamos los estilos de la carpeta styles
-
+import './styles/main.scss'; // acá estamos trayendo los estilos del main para que los loaders de webpack puedan procesarlo
 import Pokedex from './pokedex';
 import axios from 'axios';
 'use strict';
 
 // Traer todos los pokemones
-const init = () => {
-    // TODO: Consultar API
-
-    axios.get('https://pokeapi.co/api/v2/pokemon?offset=0&limit=100')
+const init = () => { 
+    axios.get('https://pokeapi.co/api/v2/pokemon?offset=0&limit=99')
         .then(response => {
-            
+
             // Obtenemos la lista de pokemones incompleta
             const pokemonesAPI = response.data.results;
-            //Pedimos la data completa de  todos los pokemones,
+
+            // Pedimos la data completa de todos los pokemones,
             // y guardamos los objetos 'promise' en una variable
-            
-            let promises = pokemonesAPI.map((pokemon) =>{
-                // const url = pokemon.url;
-                const { url }  = pokemon;            
-            return axios.get(url).then(response => response.data);
-        //    this.buildPokemonCard(pokemon)
+            let promises = pokemonesAPI.map((pokemon) => {
+              // const url = pokemon.url;
+              const { url } = pokemon;
+              return axios.get(url).then(response => response.data);
             });
-            //esperamos a que todas las promesas terminen
+
+            // Esperamos a que todas las promesas terminen
             Promise.all(promises)
-            .then(objects => {
-                console.log('Pokemones detallados',objects)
-               const pokedex = new Pokedex(objects)
-               pokedex.renderPokemonsAsCards()
-    
-            })   
-            
+              .then(objects => {
+                // console.log('Pokemones detallados...', objects);
+                const pokedex = new Pokedex(objects)
+                pokedex.renderPokemonsAsCards();
+              })
         })
-        .catch(function (error) {
-            // handled error
-            alert('no funciono  ):');
-            console.log(error);
+        .catch(function (err) {
+            alert('No funciono u_U');
+            console.log(err);
         });
-        
-
-    
-
 };
- 
 // Renderizar cada pokemon
 
-// por pokemon vamos a activar un modal para visualizarlos
+// por pokemon vamos a activar un modal para visualizarlo
 
 init();
+
